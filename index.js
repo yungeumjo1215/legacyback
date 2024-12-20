aab45f20b2e07f405641a2fd6684d50ee718bc71
yungeumjo1215 <elec0807@gmail.com>
2024-12-20 16:36:00 +0900

    index

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require("express");
-
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-
 const path = require("path");
 const spawn = require("child_process").spawn;
-
 // 라우트 임포트
 const accountRoutes = require("./routes/accountRoutes");
 const pgdbRoutes = require("./routes/postgreSQLRoutes");
 const eventRoutes = require("./routes/eventRoutes");
-
 // Swagger 설정
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./swagger.yaml");
-
 const PORT = 8000;
 const app = express();
-
 // 미들웨어 설정
 dotenv.config();
 app.use(bodyParser.json());
@@ -31,7 +25,6 @@ app.use(
     credentials: true,
   })
 );
-
 // Swagger 설정
 app.use(
   "/api-docs",
@@ -44,29 +37,24 @@ app.use(
     customSiteTitle: "문화재/축제 API 문서",
   })
 );
-
 // 기본 라우트
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
-
 // 챗봇 라우트
 app.post("/chat", async (req, res) => {
   try {
     const { question } = req.body;
-
     // 추가 함
-    const pythonPath = path.join(
-      "/home/ubuntu/miniconda",
-      "envs",
-      "myenv",
-      "bin",
-      "python3"
-    );
-    const scriptPath = path.join(__dirname, "chatbot.py");
-
-    const result = spawn(pythonPath, [scriptPath, question]);
-
+    // const pythonPath = path.join(
+    //   "/home/ubuntu/miniconda",
+    //   "envs",
+    //   "myenv",
+    //   "bin",
+    //   "python3"
+    // );
+    const scriptPath = path.join(__dirname, "chatbot", "chatbot.py");
+    // const result = spawn(pythonPath, [scriptPath, question]);
     const phythonPath = path.join(
       "/home/ubuntu/miniconda",
       "envs",
@@ -74,21 +62,16 @@ app.post("/chat", async (req, res) => {
       "bin",
       "python3"
     );
-
     let answer = "";
-    let hasResponse = false;
-
+    // let hasResponse = false;
     // Python 스크립트 실행
-    // const pythonProcess = spawn("python", [scriptPath, question]);
-
+    const pythonProcess = spawn(phythonPath, [scriptPath, question]);
     pythonProcess.stdout.on("data", (data) => {
       answer += data.toString();
     });
-
     pythonProcess.stderr.on("data", (data) => {
       console.error(`Python 에러: ${data}`);
     });
-
     pythonProcess.on("close", (code) => {
       if (code !== 0) {
         return res
@@ -102,7 +85,6 @@ app.post("/chat", async (req, res) => {
     res.status(500).json({ error: "서버 오류가 발생했습니다." });
   }
 });
-
 // API 라우트
 // app.use("/festival", festivalRoutes);
 app.use("/api/pgdb", pgdbRoutes);
@@ -111,13 +93,11 @@ app.use("/api/account", accountRoutes);
 app.use("/pgdb", pgdbRoutes);
 app.use("/event", eventRoutes);
 app.use("/account", accountRoutes);
-
 // 에러 핸들링 미들웨어
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "서버 오류가 발생했습니다." });
 });
-
 // 서버 시작
 app.listen(PORT, () => {
   console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);

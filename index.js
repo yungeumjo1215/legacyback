d241dd9ef97d14369d9e86bc9acdd7cb7ebe2cd0
yungeumjo1215 <elec0807@gmail.com>
2024-12-23 13:52:45 +0900

    d

--- index.js
+++ index.js
@@ -19,7 +19,12 @@ const app = express();
 dotenv.config();
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://legacyfront.vercel.app",
+    credentials: true,
+  })
+);
 // Swagger 설정
 app.use(
   "/api-docs",
